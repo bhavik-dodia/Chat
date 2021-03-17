662baa31f970a82b92f7bbd90d6850b0044be904
bhavik-dodia <dodiabhavik.db@gmail.com>
2021-03-18 00:38:05 +0530

    connected firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'screens/home_page.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,11 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(title: Text('Chats'),),
-body: Container(),
+      appBar: AppBar(title: Text('Chats')),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/7wJd21GWG5l87CuvJNYg/messages')
+            .snapshots(),
+        builder: (context, snapshot) {
+          final docs = snapshot.data.docs;
+          return snapshot.connectionState == ConnectionState.waiting
+              ? Text('loading')
+              : ListView.builder(
+                  itemCount: docs.length,
+                  itemBuilder: (context, index) => ListTile(
+                    title: Text(docs[index]['text']),
+                  ),
+                );
+        },
+      ),
     );
   }
 }
